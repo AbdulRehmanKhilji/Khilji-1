7af56ad347a36644d6f4123026f7b92225e4abfd
AbdulRehmanKhilji <arkhilji@yahoo.co.uk>
2021-01-09 12:51:32 +0500

    Two new lines have been added

--- Testing.r
+++ Testing.r
@@ -1,3 +1,8 @@
 # This is from Github
-Print('اللہ اکبر')
+print('اللہ اکبر')
 print('Hello! from Github')
+name="عبدالرحمن"
+version="RStudio in Writing"
+print(name)
+print("These lines have been written in RStudio")
+print(version)
--- testing-2.R
+++ testing-2.R
@@ -1,2 +1,6 @@
 print("This file was created within RStudio")
 print("And now it lives on GitHub")
+name=Abdul Rehman
+print(name)
+# new lines added
+print("Knowledge Studio")
